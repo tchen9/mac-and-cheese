31698cf80987a44bca494c72cbbfd427ef9c6cf1
alien981 <dsparakis@stuy.edu>
2017-01-11 08:23:40 -0500

    Changed private void toString to private String toString

---
+++ ConnectFour.java
@@ -0,0 +1,25 @@
+public class ConnectFour{
+
+    private int[][] Playboard = new int[7][6];
+    private boolean Victory;
+    private int[] Available = new int[6];
+
+    public connectFour(){
+	for(int[] q: Playboard){
+	    for(int r: q){
+		r = 0;
+	    }
+	}
+    }
+
+    private String toString(){
+	String answer = "";
+	for(int[] q: Playboard){
+	    for(int r: q){
+		answer += "|" + r + "|";
+	    }
+	    answer += "\n";
+	}
+    }
+
+}
