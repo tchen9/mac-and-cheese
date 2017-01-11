d6d821084eba9c2c1c40218322a072fcbfceccc0
alien981 <dsparakis@stuy.edu>
2017-01-11 08:41:52 -0500

    place method still doesn't work

--- ConnectFour.java
+++ ConnectFour.java
@@ -4,7 +4,7 @@ public class ConnectFour{
     private boolean Victory;
     private int[] Available = new int[6];
 
-    public connectFour(){
+    public void  connectFour(){
 	for(int[] q: Playboard){
 	    for(int r: q){
 		r = 0;
@@ -12,7 +12,7 @@ public class ConnectFour{
 	}
     }
 
-    private String toString(){
+    public String toString(){
 	String answer = "";
 	for(int[] q: Playboard){
 	    for(int r: q){
@@ -20,11 +20,31 @@ public class ConnectFour{
 	    }
 	    answer += "\n";
 	}
+	return answer;
     }
 
     private int[] place(int column){
+	int[] answer = new int[2];
 	int r = 0;
-	
+	while(Playboard[column][r] != 0 && r<6){
+	    r++;
+	}
+	if(r == 6){
+	    answer[0] = column;
+	    answer[1] = -1;
+	}
+	else{
+	    Playboard[column][r] = 1;
+	}
+	return answer;
+    }
+
+    public static void main(String[] args){
+	ConnectFour game = new ConnectFour();
+	System.out.println(game);
+	game.place(5);
+	System.out.println(game);
+
     }
 
 }
