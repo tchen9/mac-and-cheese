b802128e304080c5a8bb411b3fd7ba0564e25527
alien981 <dsparakis@stuy.edu>
2017-01-12 07:46:09 -0500

    Place still doesn't work

--- ConnectFour.java
+++ ConnectFour.java
@@ -1,7 +1,7 @@
 public class ConnectFour{
 
-    private int[][] Playboard = new int[7][6];
-    private boolean Victory;
+    private int[][] Playboard = new int[6][7];
+	private boolean Victory;
     private int[] Available = new int[6];
 
     public void  connectFour(){
@@ -11,40 +11,58 @@ public class ConnectFour{
 	    }
 	}
     }
+	
+	public void populate(){
+		for(int r = 0; r < 6; r++){
+			for(int q = 0; q < 7; q++){
+				Playboard[r][q] = q;
+			}
+		}
+	}
 
     public String toString(){
 	String answer = "";
-	for(int[] q: Playboard){
-	    for(int r: q){
-		answer += "|" + r + "|";
-	    }
-	    answer += "\n";
+	for(int r = 0; r < 7; r++){
+		for(int[] q: Playboard){
+			answer += "|" + q[r] + "|";
+		}
+		answer += "\n";
 	}
 	return answer;
     }
-
+	
     private int[] place(int column){
-	int[] answer = new int[2];
-	int r = 0;
-	while(Playboard[column][r] != 0 && r<6){
-	    r++;
-	}
-	if(r == 6){
-	    answer[0] = column;
-	    answer[1] = -1;
-	}
-	else{
-	    Playboard[column][r] = 1;
-	}
-	return answer;
+		int[] answer = new int[2];
+		int r = 0;
+		while(Playboard[r][column] == 0){
+			r++;
+		}
+		if(Playboard[0][column] != 0){
+			answer[0] = -1;
+			answer[1] = -1;
+		}
+		else{
+			Playboard[r - 1][column] = 1;
+			answer[0] = r - 1;
+			answer[1] = column;
+		}
+		return answer;
     }
+	
+	public static String toString(int[] intArray){
+		String answer = "[";
+		for( int r: intArray){
+			answer += r + ", ";
+		}
+		answer += "]";
+		return answer;
+	}
 
     public static void main(String[] args){
 	ConnectFour game = new ConnectFour();
 	System.out.println(game);
-	game.place(5);
-	System.out.println(game);
-
+	game.place(1);
+	System.out.println(game.Playboard);
     }
 
 }
