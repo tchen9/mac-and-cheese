884cd56c244c3f2329959714b1292e53d154e80d
Max Chan <m2000chan@141.78.119.52.naturalwireless.com>
2017-01-13 07:15:08 -0500

    Making sure things worked, tweaked blackjack a little

--- HighLow.java
+++ HighLow.java
@@ -1,5 +1,13 @@
 public class HighLow{
     public static boolean play(){
-	return true;
+	    if (Math.random() > 0.5) {
+            System.out.println("Congradulations! You won!!");
+            return true;
+            
+        }
+        else{
+            System.out.println("Sorry! You lost!!");
+            return false;
+        }
     }
 }
