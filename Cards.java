681ee0160eb979ef85feacb67bb2e1ce80fb7ac2
Max Chan <m2000chan@76.78.119.52.naturalwireless.com>
2017-01-15 18:40:04 -0500

    Created class cards

---
+++ Cards.java
@@ -0,0 +1,19 @@
+public class Cards{
+    
+    public ArrayList<Object> deck = new ArrayList();
+    
+    public void createDeck(){
+        for (int x = 2; x < 11; x++){
+            for (int y = 0; y < 4; y++){
+            deck.add(x);
+            //System.out.println(deck);
+            }
+        }
+        for (int z = 0; z < 4; z++){
+            deck.add("King");
+            deck.add("Queen");
+            deck.add("Jack");
+            deck.add("Ace");
+        }
+    }
+}
