9a672f5e487b44b02660fc34c5e8754447031453
Max Chan <m2000chan@76.78.119.52.naturalwireless.com>
2017-01-15 00:16:18 -0500

    Finished base case blackjack

--- blackjack.java
+++ blackjack.java
@@ -17,7 +17,7 @@ public class blackjack{
         return true;
     }
     
-    public boolean playHelp(){
+    public String playHelp(){
         createDeck();
         String s;
         s = "Your Hand: ";
@@ -40,10 +40,10 @@ public class blackjack{
         s = "Would you like to hit, or stay? (Enter h for hit, s for stay)";
         System.out.println(s);
         while (Keyboard.readString().equals("h")){
-            hit();
+            playerHit();
             if (playerCount > 21){
                 System.out.println("Sorry, you lose! Game Over…");
-                return false;
+                return "Lose";
             }
             else{
                 s = "Do you want to hit again? (Enter h for hit, s for stay)";
@@ -52,8 +52,58 @@ public class blackjack{
             
             
         }
-        return true;
+        
+        s = "Your Hand: ";
+        System.out.print(s);
+        System.out.println(playerHand);
+        s = "Your cards add up to ";
+        System.out.print(s);
+        System.out.println(playerCount);
+        
+        System.out.println("Dealer's turn . . .");
+        
+        while (dealerCount <= 16){
+            s = "\nDealer's hand is " + dealerHand;
+            System.out.println(s);
+            dealerHit();
         }
+        
+        s = "Your Hand: ";
+        System.out.print(s);
+        System.out.println(playerHand);
+        s = "Your cards add up to ";
+        System.out.print(s);
+        System.out.println(playerCount);
+        
+        s = "Dealer's Hand: ";
+        System.out.print(s);
+        System.out.println(dealerHand);
+        s = "Dealer's cards add up to ";
+        System.out.print(s);
+        System.out.println(dealerCount);
+        
+        if (dealerCount > 21){
+            System.out.println("Congratulations!! You win!");
+            return "Win";
+        }
+        
+        if (dealerCount > playerCount){
+            System.out.println("Sorry, you lost! Come back again");
+            return "Lose";
+        }
+        
+        if (dealerCount == playerCount){
+            System.out.println("Draw! You and the dealer have the same card value");
+            return "Draw";
+        }
+        
+        else{
+            System.out.println("Congratulations!! You win!");
+            return "Win";
+        }
+            
+        
+    }
 
     public Object pickcard(){
         int index = (int) (Math.random() * deck.size());
@@ -97,19 +147,31 @@ public class blackjack{
     }
 	
     public static void deal(){}
-    public void hit(){
+    public Object playerHit(){
         Object card = pickcard();
         System.out.println("Your new card is: " + card);
         playerHand.add(card);
         System.out.print("Your hand is now: ");
         System.out.println(playerHand);
         playerCount = countCards(playerHand);
+        return card;
     }
+    
+    public Object dealerHit(){
+        Object card = pickcard();
+        System.out.println("Dealer's new card is: " + card);
+        dealerHand.add(card);
+        System.out.print("Dealer's hand is now: ");
+        System.out.println(dealerHand);
+        dealerCount = countCards(dealerHand);
+        return card;
+    }
+    
     public static void dealerMove(){}
     
-    public static void main(String[] args){
+    /*public static void main(String[] args){
         blackjack Frank = new blackjack();
         Frank.playHelp();
-    }
+    }*/
 
 }
