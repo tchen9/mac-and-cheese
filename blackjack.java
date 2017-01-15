3cd582db4c125b29320be73187026c02bc4264af
Max Chan <m2000chan@76.78.119.52.naturalwireless.com>
2017-01-14 23:45:16 -0500

    updating blackjack

--- blackjack.java
+++ blackjack.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import cs1.Keyboard;
 
 public class blackjack{
     private int dealerCount;
-    private String dealerHand;
-    private int playCount;
-    private String playerHand;
+    private ArrayList dealerHand = new ArrayList();
+    private int playerCount;
+    private ArrayList playerHand = new ArrayList();
     //private static ArrayList<Object> deck;
 
     
@@ -17,29 +18,42 @@ public class blackjack{
     }
     
     public boolean playHelp(){
-        //ArrayList<Object> deck = new ArrayList();
         createDeck();
         String s;
         s = "Your Hand: ";
-        s += pickcard();
-        s += ", ";
-        s += pickcard();
-        System.out.println(s);
+        playerHand.add(pickcard());
+        playerHand.add(pickcard());
+        playerCount = countCards(playerHand);
+        System.out.print(s);
+        System.out.println(playerHand);
+        //System.out.println(playerCount);
         s = "\nDealer's Hand: ";
-        s += pickcard();
-        s += ", ";
-        s += "UNKNOWN";
+        System.out.print(s);
+        dealerHand.add(pickcard());
+        System.out.print(dealerHand);
+        dealerHand.add(pickcard());
+        dealerCount = countCards(dealerHand);
+        //System.out.println(dealerCount);
+        //System.out.println(dealerHand);
+        s = "(Other card unknown)";
         System.out.println(s);
-	    if (Math.random() > 0.5) {
-            System.out.println("Congradulations! You won!!");
-            return true;
+        s = "Would you like to hit, or stay? (Enter h for hit, s for stay)";
+        System.out.println(s);
+        while (Keyboard.readString().equals("h")){
+            hit();
+            if (playerCount > 21){
+                System.out.println("Sorry, you lose! Game Over…");
+                return false;
+            }
+            else{
+                s = "Do you want to hit again? (Enter h for hit, s for stay)";
+                System.out.println(s);
+            }
+            
             
         }
-        else{
-            System.out.println("Sorry! You lost!!");
-            return false;
+        return true;
         }
-    }
 
     public Object pickcard(){
         int index = (int) (Math.random() * deck.size());
@@ -63,17 +77,39 @@ public class blackjack{
             deck.add("Ace");
         }
     }
+    
+    public int countCards(ArrayList thing){
+        int counter = 0;
+        for (Object x: thing){
+            if (x instanceof Integer){
+                counter += (int) x;
+            }
+            else{
+                if (x.equals("Ace")){
+                    counter += 1;
+                }
+                else{
+                    counter += 10;
+                }
+            }
+        }
+        return counter;
+    }
 	
     public static void deal(){}
-    public static void hit(){}
-    public static boolean stay(){
-	return true;
+    public void hit(){
+        Object card = pickcard();
+        System.out.println("Your new card is: " + card);
+        playerHand.add(card);
+        System.out.print("Your hand is now: ");
+        System.out.println(playerHand);
+        playerCount = countCards(playerHand);
     }
     public static void dealerMove(){}
     
-    /*public static void main(String[] args){
+    public static void main(String[] args){
         blackjack Frank = new blackjack();
-        Frank.play();
-    }*/
+        Frank.playHelp();
+    }
 
 }
